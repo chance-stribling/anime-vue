075877ba7870f6d7a7742e6ce39056489d61b254
chance-stribling <cstribling1717@gmail.com>
2023-06-06 05:41:10 -0400

    added new page

--- src/components/HeaderDiv.vue
+++ src/components/HeaderDiv.vue
@@ -6,7 +6,8 @@
       <div class="skew3"></div>
     </div>
     <div class="square1">
-      <div class="title">
+        <v-btn v-show="row>0" @click="grow()" icon="mdi-chevron-left" variant="plain" size="small" color="white" class="home" style="margin-left: 200px;"></v-btn>
+        <div class="title">
         <h1 class="font-weight-black">Chance Stribling</h1>
         <h3 class="font-weight-black">Web Developer</h3>
         <div
@@ -25,124 +26,30 @@
           <v-btn
             variant="elevated"
             color="white"
-            size="x-large"
+            size="large"
             @click="shrink()"
             class="nav-button"
             >Experience</v-btn
           >
-          <v-btn variant="elevated" color="white" size="x-large" class="nav-button">Skills</v-btn>
-          <v-btn variant="elevated" color="white" size="x-large" class="nav-button">Projects</v-btn>
+          <v-btn variant="elevated" @click="shrink2()" color="white" size="large" class="nav-button">Skills</v-btn>
+          <v-btn variant="elevated" color="white" size="large" class="nav-button">Projects</v-btn>
         </div>
       </div>
     </div>
     <div class="square2">
       <h1 class="mt-15 font-weight-black">EXPERIENCE</h1>
-      <v-card class="job-list">
-        <template v-slot:title> Web Developer Intern </template>
-
-        <template v-slot:subtitle> Business Infomation Systems </template>
-
-        <v-card-actions class="justify-end">
-          <v-btn color="#adff2f" variant="text" @click="show = !show"> Details </v-btn>
-
-          <v-btn :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show = !show"></v-btn>
-        </v-card-actions>
-
-        <v-expand-transition>
-          <div v-show="show">
-            <v-divider></v-divider>
-
-            <v-card-text style="line-height: 2.5; font-size: medium; text-align: left">
-              <div class="desc">Working in an Agile Software Development lifecycle.<br /></div>
-              <div class="desc">
-                Analyzing user requirements, envisioning system features, and working with a team to
-                bring them to life. <br />
-              </div>
-              <div class="desc">
-                Designing User Interfaces and Backend Systems, including Database Design and
-                Interfacing with 3rd Party systems.<br />
-              </div>
-              <div class="desc">
-                Completing software development activities by coordinating requirements, schedules,
-                and activities.<br />
-              </div>
-              <div class="desc">
-                Contributing to team meetings and troubleshooting development and production
-                problems.<br />
-              </div>
-              <div class="desc">
-                Supporting users and in-house teams by developing documentation and assistance
-                tools.<br />
-              </div>
-              <div class="desc">
-                Continually improving job knowledge by researching applicable technologies and
-                software products.<br />
-              </div>
-              <div class="desc">
-                Enhancing organization reputation by accepting ownership for accomplishing new and
-                different requests.<br />
-              </div>
-            </v-card-text>
-          </div>
-        </v-expand-transition>
-      </v-card>
-      <v-card class="job-list">
-        <template v-slot:title> Quality Control Analyst </template>
-
-        <template v-slot:subtitle> Business Infomation Systems </template>
-
-        <v-card-actions class="justify-end">
-          <v-btn color="#adff2f" variant="text" @click="show2 = !show2"> Details </v-btn>
-
-          <v-btn :icon="show2 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show2 = !show2"></v-btn>
-        </v-card-actions>
-
-        <v-expand-transition>
-          <div v-show="show2">
-            <v-divider></v-divider>
-
-            <v-card-text style="line-height: 2.5; font-size: medium; text-align: left">
-              <div class="desc">Working in an Agile Software Development lifecycle.<br /></div>
-              <div class="desc">
-                Analyzing user requirements, envisioning system features, and working with a team to
-                bring them to life. <br />
-              </div>
-              <div class="desc">
-                Designing User Interfaces and Backend Systems, including Database Design and
-                Interfacing with 3rd Party systems.<br />
-              </div>
-              <div class="desc">
-                Completing software development activities by coordinating requirements, schedules,
-                and activities.<br />
-              </div>
-              <div class="desc">
-                Contributing to team meetings and troubleshooting development and production
-                problems.<br />
-              </div>
-              <div class="desc">
-                Supporting users and in-house teams by developing documentation and assistance
-                tools.<br />
-              </div>
-              <div class="desc">
-                Continually improving job knowledge by researching applicable technologies and
-                software products.<br />
-              </div>
-              <div class="desc">
-                Enhancing organization reputation by accepting ownership for accomplishing new and
-                different requests.<br />
-              </div>
-            </v-card-text>
-          </div>
-        </v-expand-transition>
-      </v-card>
+      <Job1 />
+      <Job2 />
+    </div>
+    <div class="square3">
+      <h1 class="mt-15 font-weight-black">SKILLS</h1>
+      <Job1 />
+      <Job2 />
     </div>
   </div>
 </template>
 
 <style scoped>
-.desc {
-  font-weight: medium;
-}
 .skew {
   transform: translateY(150px) skewY(-10deg);
   height: 80px;
@@ -170,23 +77,14 @@
   text-align: center;
   overflow: visible;
 }
-.desc:hover {
-  scale: 1.01;
-  color: #adff2f;
-}
+
 .nav-menu {
-  margin-top: 50px;
+  margin-top: 25px;
 }
 .nav-button {
   width: 140px;
 }
-.job-list {
-  min-height: 100px;
-  width: 70vw;
-  margin: 15px auto;
-  background-color: black;
-  color: white;
-}
+
 .main {
   display: flex;
   flex-direction: row;
@@ -209,12 +107,23 @@
   text-align: center;
   color: black;
 }
+.square3 {
+  opacity: 0;
+  width: 0;
+  max-height: 100vh;
+  overflow-x: hidden;
+
+  background-color: white;
+  text-align: center;
+  color: black;
+}
 .title {
   color: white;
   font-size: 25px;
   margin: 40vh 0;
 }
-.title h1,h3 {
+.title h1,
+h3 {
   color: white;
   -webkit-text-stroke-width: 1px;
   -webkit-text-stroke-color: black;
@@ -223,12 +132,16 @@
 
 <script>
 import anime from 'animejs/lib/anime.es'
+import Job1 from '../components/Jobs/Job1.vue'
+import Job2 from '../components/Jobs/Job2.vue'
 
 export default {
+  components: {
+    Job1,
+    Job2
+  },
   data() {
     return {
-        show: false,
-        show2: false,
       row: 0
     }
   },
@@ -264,15 +177,83 @@ export default {
         anime({
           targets: '.square2',
           easing: 'linear',
-          position: 'relateive',
+          position: 'relative',
+          opacity: 100,
+          height: '100%',
+          width: '75vw',
+          duration: 1000,
+          loop: false
+        }),
+      anime({
+        targets: ['.square3'],
+        width: 0,
+        opacity:0,
+        easing: 'easeInOutQuad',
+        loop: false,
+        duration: 2000
+      })
+    },
+    shrink2: function () {
+      this.row++
+      anime({
+        targets: ['.square1', '.bg-animation', '.skew', '.skew2', '.skew3'],
+        width: '25vw',
+        easing: 'easeInOutQuad',
+        loop: false,
+        duration: 2000
+      }),
+        anime({
+          targets: '.title h1',
+          easing: 'linear',
+          fontSize: '30px',
+          direction: 'alternate',
+          duration: 1000,
+          loop: false
+        }),
+        anime({
+          targets: '.title h3',
+          easing: 'linear',
+          fontSize: '16px',
+          direction: 'alternate',
+          duration: 1000,
+          loop: false
+        }),
+        anime({
+          targets: '.square3',
+          easing: 'linear',
+          position: 'relative',
           opacity: 100,
           height: '100%',
           width: '75vw',
           duration: 100,
           loop: false
-        })
+        }),
+      anime({
+        targets: ['.square2'],
+        width: '0',
+        easing: 'easeInOutQuad',
+        loop: false,
+        duration: 2000
+      })
+        
     },
-
+    grow: function () {
+      this.row=0;
+      anime({
+        targets: ['.square1', '.bg-animation', '.skew', '.skew2', '.skew3'],
+        width: '100vw',
+        easing: 'easeInOutQuad',
+        loop: false,
+        duration: 2000
+      }),
+      anime({
+        targets: ['.square2', '.square3'],
+        width: '0',
+        easing: 'easeInOutQuad',
+        loop: false,
+        duration: 2000
+      })
+    },
     go: function () {
       anime({
         targets: ['.square1', '.bg-animation', '.skew', '.skew2', '.skew3'],
---
+++ src/components/Jobs/Job1.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-card class="job-list">
+    <template v-slot:title> Web Developer Intern </template>
+
+    <template v-slot:subtitle> Business Infomation Systems </template>
+
+    <v-card-actions class="justify-end">
+      <v-btn color="#adff2f" variant="text" @click="show = !show"> Details </v-btn>
+
+      <v-btn :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show = !show"></v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text style="line-height: 2.5; font-size: medium; text-align: left">
+          <div class="desc">Working in an Agile Software Development lifecycle.<br /></div>
+          <div class="desc">
+            Analyzing user requirements, envisioning system features, and working with a team to
+            bring them to life. <br />
+          </div>
+          <div class="desc">
+            Designing User Interfaces and Backend Systems, including Database Design and Interfacing
+            with 3rd Party systems.<br />
+          </div>
+          <div class="desc">
+            Completing software development activities by coordinating requirements, schedules, and
+            activities.<br />
+          </div>
+          <div class="desc">
+            Contributing to team meetings and troubleshooting development and production
+            problems.<br />
+          </div>
+          <div class="desc">
+            Supporting users and in-house teams by developing documentation and assistance tools.<br />
+          </div>
+          <div class="desc">
+            Continually improving job knowledge by researching applicable technologies and software
+            products.<br />
+          </div>
+          <div class="desc">
+            Enhancing organization reputation by accepting ownership for accomplishing new and
+            different requests.<br />
+          </div>
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+<script>
+import anime from 'animejs/lib/anime.es'
+
+export default {
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
